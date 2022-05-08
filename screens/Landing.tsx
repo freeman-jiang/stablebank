f445ddc9c61986625d067b0d6d7df86a06c801af
Freeman Jiang <freeman.jiang.ca@gmail.com>
2022-05-07 23:35:01 -0700

    Add colored Google icon

--- screens/Landing.tsx
+++ screens/Landing.tsx
@@ -15,6 +15,7 @@ import { StatusBar } from "react-native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../App";
 import { useLang } from "../context/lang";
+import Google from "../assets/google.png";
 type Props = NativeStackScreenProps<RootStackParamList, "Landing">;
 
 export const Landing = ({ navigation }: Props) => {
@@ -88,8 +89,9 @@ export const Landing = ({ navigation }: Props) => {
             </Text>
           </Button>
           <IconButton
+            onPress={() => navigation.navigate("Dashboard")}
             //@ts-ignore
-            icon={<FontAwesome name="apple" size={22} color="white" />}
+            icon={<FontAwesome name="apple" size={28} color="white" />}
             bg="gray.700"
             w="20%"
             rounded="3xl"
@@ -97,16 +99,23 @@ export const Landing = ({ navigation }: Props) => {
               bg: "gray.600",
             }}
           />
-          <IconButton
-            //@ts-ignore
-            icon={<AntDesign name="google" size={22} color="white" />}
+          <Button
+            onPress={() => navigation.navigate("Dashboard")}
+            color="white"
             bg="gray.700"
             w="20%"
             rounded="3xl"
             _pressed={{
               bg: "gray.600",
             }}
-          />
+          >
+            <Image
+              source={Google}
+              boxSize={6}
+              resizeMode="contain"
+              alt="Google Logo"
+            />
+          </Button>
         </HStack>
         <HStack mt={8} justifyContent="flex-end"></HStack>
       </VStack>
