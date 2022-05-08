77f479a2d248b555a2a58e380196041fe7ed58d4
Freeman Jiang <freeman.jiang.ca@gmail.com>
2022-05-08 01:07:06 -0700

    Add screen width and height util

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
+import { Dimensions } from "react-native";
 export const getInitials = (fullName: string): string => {
   const fullNameArray = fullName.split(" ");
   if (fullNameArray.length === 0) {
@@ -6,4 +7,12 @@ export const getInitials = (fullName: string): string => {
   const initials =
     fullNameArray.shift()!.charAt(0) + fullNameArray.pop()!.charAt(0);
   return initials.toUpperCase();
+};
+
+export const getScreenHeight = () => {
+  return Dimensions.get("window").height;
+};
+
+export const getScreenWidth = () => {
+  return Dimensions.get("window").width;
 };
